145294d5cc654e7873fb268709c81e5fcc3ccee0
Ananthapadmanabhan M <ananthapadmanabhanr2@gmail.com>
2025-02-15 14:53:32 +0530

    Created the client side

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h2 className='text-red-800'>
+        Welcome to the React App
+      </h2>
+
+    </div>
+  )
+}
