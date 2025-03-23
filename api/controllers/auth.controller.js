06c83740facce2487fc3265fd0ebacc19231c98e
Ananthapadmanabhan M <ananthapadmanabhanr2@gmail.com>
2025-03-23 14:57:17 +0530

    passing user data to the cookies instead of a message

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -65,13 +65,15 @@ export const login = async (req,res) => {
             id:user
         }, process.env.JWT_SECRET_KEY,
         {expiresIn: age}
-    )
+    );
+
+    const {password: userPassword, ...userInfo} = user;
 
         res.cookie("token", token, {
             httpOnly:true,
             // secure:true,
             maxAge: age,
-        }).status(200).json({message:"Login Successful"})
+        }).status(200).json(userInfo)
     } catch (error) {
         console.log(error.message);
         
