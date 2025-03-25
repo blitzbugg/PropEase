bac279d60596ea758e94a7bc838f7d4952b5e99a
Ananthapadmanabhan M <ananthapadmanabhanr2@gmail.com>
2025-03-25 20:01:40 +0530

    Implemented the basic CRUD operations

--- api/app.js
+++ api/app.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import authRoute from './routes/auth.route.js'
 import postRoute from './routes/post.route.js';
 import testRoute from './routes/test.route.js';
+import userRoute from './routes/user.route.js';
 
 const app = express();
 
@@ -13,8 +14,9 @@ app.use(express.json())
 app.use(cookieParser())
 dotenv.config();
 
-app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
 
 app.listen(8800, () => {
